535de624e38b0e9e216391b52d562420dd380358
Mohnish Sharma <mohnish.unity3d.dev@gmail.com>
2022-10-29 06:32:31 +0530

    Update CMake files and build scripts for project generation

--- build_scripts/windows/create_project.ps1
+++ build_scripts/windows/create_project.ps1
@@ -1,6 +1,9 @@
 . $PSScriptRoot\_variables.ps1
 
+# Required arguments
 $projNameArg = $args[0]
+$chapterNumberArg = $args[1]
+
 if($null -eq $projNameArg) {
     Write-Warning "no project name given. Creating template 'tempproj'"
     $projNameArg = "tempproj"
@@ -16,9 +19,8 @@ mkdir src
 touch .\src\main.cpp
 touch CMakeLists.txt
 
-$chapterNumberArg = $args[1]
 $chapterNumber = ""
-if($chapterNumberArg -eq $null) {
+if($null -eq $chapterNumberArg) {
     $chapterNumber = "##"
 } else {
     $chapterNumber = $chapterNumberArg
@@ -28,7 +30,11 @@ Add-Content -Path .\CMakeLists.txt -Value "cmake_minimum_required(VERSION 3.22.0
 Add-Content -Path .\src\main.cpp -Value "#include <iostream>`n`nint main() {`n    std::cout << `"Hello, World! from $projNameArg\n`";`n    return 0;`n}"
 Add-Content -Path $PROJECT_ROOT_DIR_PATH\CMakeLists.txt -Value "`nadd_subdirectory(src/Chapter$chapterNumber/$projNameArg)"
 
-if($chapterNumberArg -ne $null) {
-    cd ..
-    mv .\$projNameArg "$PROJECT_ROOT_DIR_PATH\src\Chapter$chapterNumber"
+if($null -ne $chapterNumberArg) {
+    Set-Location ..
+    $projectPath = "$PROJECT_ROOT_DIR_PATH\src\Chapter$chapterNumber"
+    if((Test-Path -Path $projectPath) -eq $false) {
+        mkdir $projectPath
+    }
+    Move-Item .\$projNameArg $projectPath
 }

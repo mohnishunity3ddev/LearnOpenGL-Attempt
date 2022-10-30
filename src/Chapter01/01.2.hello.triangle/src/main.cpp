2c0f243e880a6e18749e4efebfe1b3653d0a4da5
Mohnish Sharma <mohnish.unity3d.dev@gmail.com>
2022-10-30 05:48:01 +0530

    Completed Hello Triangle. Render Rectangle. Enable Wireframe Mode

--- src/Chapter01/01.2.hello.triangle/src/main.cpp
+++ src/Chapter01/01.2.hello.triangle/src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 
+void toggleWireframeMode();
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
@@ -10,10 +11,18 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+bool wireframeMode = false;
+
 float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f,
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
+};
+
+unsigned int indices[] = {
+    0, 1, 3,    // first triangle
+    1, 2, 3     // second triangle
 };
 
 const char *vertexShaderSource = "#version 330 core\n"
@@ -91,14 +100,22 @@ int main()
     glDeleteShader(fragmentShader);
 
     // Vertex Data
-    unsigned int VBO;
+    unsigned int VBO, VAO;
     glGenBuffers(1, &VBO);
-    unsigned int VAO;
     glGenVertexArrays(1, &VAO);
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // The last element buffer object that gets bound while a VAO is bound, is stored as the VAO's element buffer object. 
+    // Binding to a VAO then also automatically binds that EBO.
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -112,20 +129,45 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // The last argument here is the offset to the first vertex index in the elements buffer EBO. 
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
     glfwTerminate();
     return 0;
+}
+
+void toggleWireframeMode() {
+    wireframeMode = !wireframeMode;
+    GLenum mode = wireframeMode ? GL_LINE : GL_FILL;
+    const char* str = wireframeMode ? "Wireframe Mode ON" : "Wireframe Mode OFF";
+    glPolygonMode(GL_FRONT_AND_BACK, mode);
+    std::cout << str << "\n";
 }
 
 void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    
+    int wKeyStatus = glfwGetKey(window, GLFW_KEY_W);
+    static bool wKeyPressed = false;
+    if (!wKeyPressed && wKeyStatus == GLFW_PRESS) {
+        wKeyPressed = true;
+        toggleWireframeMode();
+    }
+    if (wKeyStatus == GLFW_RELEASE) {
+        wKeyPressed = false;
+    }
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)

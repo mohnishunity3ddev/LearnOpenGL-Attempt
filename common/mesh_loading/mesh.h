d13ae9d926f98f9d014aff534767fa69203e1365
Mohnish Sharma <mohnish.unity3d.dev@gmail.com>
2022-11-10 00:20:02 +0530

    Section 3.2 Mesh class added

---
+++ common/mesh_loading/mesh.h
@@ -0,0 +1,39 @@
+#ifndef __JMESH_H
+#define __JMESH_H
+
+#include <glm/glm.hpp>
+#include <string>
+#include <vector>
+
+#include <shader/shader.h>
+
+struct VertexAttribute {
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec3 texCoords;
+};
+
+struct MeshTexture {
+    // id of the texture
+    unsigned int id;
+    // type of the shader. diffuse/specular
+    std::string type;
+};
+
+class Mesh {
+public:
+    // mesh
+    std::vector<VertexAttribute> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<MeshTexture> textures;
+
+    Mesh(const std::vector<VertexAttribute> &vertices, const std::vector<unsigned int> &indices, const std::vector<MeshTexture> &textures);
+    void Draw(const Shader& shader);
+
+private:
+    // render data
+    unsigned int VAO, VBO, EBO;
+    void setupMesh();
+};
+
+#endif

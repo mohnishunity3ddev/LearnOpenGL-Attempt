7d940cff84384411b4234ef012f5313bd571241d
Mohnish Sharma <mohnish.unity3d.dev@gmail.com>
2022-11-18 16:31:42 +0530

    Update build scripts for VS Builds

--- build_scripts/windows/build_msvc.ps1
+++ build_scripts/windows/build_msvc.ps1
@@ -16,26 +16,26 @@ if([bool]$arg) {
 
 Invoke-Expression "& cmake -S '$PROJECT_ROOT_DIR_PATH' -B '$BIN_DIR_PATH'"
 
-if($?) {
-    if($build -eq 1) {
-        Write-Alert "#####Building All MSVC Projects ..."
-        Invoke-Expression "& MSBuild.exe '$BIN_DIR_PATH\$PROJECT_NAME.sln' "
+# if($?) {
+#     if($build -eq 1) {
+#         Write-Alert "#####Building All MSVC Projects ..."
+#         Invoke-Expression "& MSBuild.exe '$BIN_DIR_PATH\$PROJECT_NAME.sln' "
         
-        if($?) {
-            Write-Output "#####Build Successful."
-            $arg2 = $args[1]
-            if([bool]$arg2 -and (($arg2 -eq "start") -or ($arg2 -eq "s") -or ($arg2 -eq "1") )) {
-                Invoke-Expression "& '$BIN_DIR_PATH\$PROJECT_NAME.sln'"
-            }
-        } else {
-            Write-Error "#####Build Failed."
-        }
-    } elseif([bool]$arg) {
-        Write-Warning "Argument provided. Pass 'b' / 'build' / 'force' / 'f' to build all vs projects. Skipping for now ..."
-    }
-} else {
-    Write-Error "#####Configuration failed."
-}
+#         if($?) {
+#             Write-Output "#####Build Successful."
+#             $arg2 = $args[1]
+#             if([bool]$arg2 -and (($arg2 -eq "start") -or ($arg2 -eq "s") -or ($arg2 -eq "1") )) {
+#                 Invoke-Expression "& '$BIN_DIR_PATH\$PROJECT_NAME.sln'"
+#             }
+#         } else {
+#             Write-Error "#####Build Failed."
+#         }
+#     } elseif([bool]$arg) {
+#         Write-Warning "Argument provided. Pass 'b' / 'build' / 'force' / 'f' to build all vs projects. Skipping for now ..."
+#     }
+# } else {
+#     Write-Error "#####Configuration failed."
+# }
 
 
 
